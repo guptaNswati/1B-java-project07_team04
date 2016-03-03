4ec335e51c03b0c91fb8e8dd7d8b107fdd1abdd5
Shiva Upadhye <shivaa03@yahoo.com>
2016-03-03 13:19:29 -0800

    Created ColoredPoint class.

---
+++ src/ColoredPoint.java
@@ -0,0 +1,23 @@
+import java.awt.Color;
+import java.awt.Point;
+
+public class ColoredPoint extends Point {
+	private Color color;
+	private double originalX;
+	private double originalY;
+	
+	public ColoredPoint(Color color, double originalX, double originalY, int mappedX, int mappedY){
+		super(mappedX, mappedY);
+		this.color = color;
+		this.originalX = originalX;
+		this.originalY = originalY;
+	}
+
+	public Color getColor() {
+		return color;
+	}
+
+	public String getLabel(){
+		return super.toString();
+	}
+}
