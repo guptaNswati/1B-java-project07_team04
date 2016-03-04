4579c5bb60e2b591df4885e3209cccbfbc8e6781
swati gupta <sgupta1505@gmail.com>
2016-03-04 14:33:43 -0800

    Updated GraphView constructor to calculate min and max data points and values

--- src/GraphView.java
+++ src/GraphView.java
@@ -1,4 +1,5 @@
 
+import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
@@ -8,57 +9,43 @@ import javax.swing.JPanel;
 
 public class GraphView extends JPanel
 {
-   private static final int POINT_SIZE = 1;
+   private static final int POINT_SIZE = 5;
    private int width;
    private int height;
    private Font font;
+   private int minYearX;
+   private int maxYearX;
+   private double dataMinY;
+   private double dataMaxY;
+  
    
    // constructor for GraphView
    GraphView(int width, int height, LinkedList<Country> countries)
    {
        this.width = width;
        this.height = height;
-       font = new Font("Serif", Font.PLAIN, 11);  
-      
-     
-      Iterator<SubscriptionYear> iterator = countries.getNodeAtIndex(0).getData().getSubscriptions().iterator();
-      
-      int dataMinX = 9999;
-      int dataMaxX = 0;
-      
-      double dataMinY = 9999;
-      double dataMaxY = 0.0;
-      
-      
-      SubscriptionYear current = null;
-      
-      while (iterator.hasNext())
-      {
-          current = iterator.next();
-       // update the minYear as minimum year
-          if (current.getYear() < dataMinX)        
-              dataMinX = current.getYear();
+       font = new Font("Serif", Font.PLAIN, 11); 
        
-          // update the maxYear as the maximum year, each time new node is added to list
-          else if (current.getYear() > dataMaxX)        
-              dataMaxX = current.getYear();
-      }
-      
-      Country currentCountry;
-      Iterator<Country> iterator_c = countries.iterator();
-      
-      while (iterator_c.hasNext())
-      {
-          currentCountry = iterator_c.next();
-       // update the minYear as minimum year
-          if (currentCountry.getSubscriptions(). < dataMinY)        
-              dataMinX = current.getYear();
+       this.minYearX = countries.getNodeAtIndex(0).getData().getMinYear();
+       this.maxYearX = countries.getNodeAtIndex(0).getData().getMaxYear();
        
-          // update the maxYear as the maximum year, each time new node is added to list
-          else if (current.getYear() > dataMaxX)        
-              dataMaxX = current.getYear();
-      }
-      
+       dataMinY = 0.0;
+       dataMaxY = 0.0;
+       
+       Iterator <Country> iterator = countries.iterator();
+       
+       Country current = null;
+       
+       while(iterator.hasNext())
+       {
+           current = iterator.next();
+           
+           if(current.getMaxSubscription() > dataMaxY)
+               dataMaxY = current.getMaxSubscription();        
+       }
+       
+//       while(iterator)
+    
    }
    
    public static final double map(double value, double dataMin, double dataMax, double plottedMin, double plottedMax)
@@ -73,10 +60,10 @@ public class GraphView extends JPanel
    {
        super.paintComponent(g);
        Graphics2D g2d = (Graphics2D) g;
-       ColoredPoint current;
+       ColoredPoint current = new ColoredPoint(Color.BLUE, this.minYearX, this.dataMinY, 160 , 140);
        g2d.setColor(current.getColor());    
        g2d.fillOval((int)current.getX(), (int)current.getY(), POINT_SIZE, POINT_SIZE);
-       g2d.drawString(label, (int)current_point.getX(),(int)current_point.getY()); 
+       g2d.drawString(current.getLabel(), (int)current.getX(),(int)current.getY()); 
    }
 
 }
