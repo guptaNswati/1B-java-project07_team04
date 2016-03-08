7bc8ca0ab18c5e786f229bb064cc0e8fb06d4a4e
Shiva Upadhye <shivaa03@yahoo.com>
2016-03-08 13:39:31 -0800

    Created paintComponent() method in LegendPanel.

--- src/LegendPanel.java
+++ src/LegendPanel.java
@@ -3,8 +3,12 @@ import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Point;
+import java.awt.Rectangle;
+import java.util.Iterator;
+
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.Scrollable;
 import javax.swing.border.Border;
 
 /**
@@ -13,7 +17,7 @@ import javax.swing.border.Border;
  * @author Shiva
  *
  */
-public class LegendPanel extends JPanel {
+public class LegendPanel extends JPanel implements Scrollable {
 
 	private int height;
 	private int width;
@@ -31,41 +35,53 @@ public class LegendPanel extends JPanel {
 		
 	}
 	
-	/**
-	 * Nested class.
-	 * One object of type legend stores a label for the name of country
-	 * and an object of type color for holding the color
-	 */
-	public class Legend{
-		
-		private String countryName;
-		private JLabel countryLabel;
-		private Color legendColor;
-		
-		
-		public Legend(String countryName, Color legendColor){
-			this.countryLabel = new JLabel(countryName);
-			this.legendColor = legendColor;
-		}
-
-
-		public Color getLegendColor() {
-			return legendColor;
-		}
-
-
-		public void setLegendColor(Color legendColor) {
-			this.legendColor = legendColor;
-		}
-		
-		
-	}
-	
 	public void paintComponent(Graphics g){
 		Graphics2D g2d = (Graphics2D)g;
-		 
-		
-	
+		LinkedList<Legend>legends = GraphView.getListOfLegends();
+		Iterator<Legend> l_iterator = legends.iterator();
+		//Hard coded values based on 
+		int x = 173;
+		int y = 27;
+		int increment = 20;
+		Legend legendKey;
+		while(l_iterator.hasNext()){
+			legendKey = l_iterator.next();
+			for(int counter = 0; counter < legends.size(); counter++){
+				g2d.drawRect(x, y, 30, 30);
+				g2d.setColor(legendKey.getLegendColor());
+				y = y + increment;
+			}
+		}
+	}
+
+	@Override
+	public Dimension getPreferredScrollableViewportSize() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getScrollableBlockIncrement(Rectangle arg0, int arg1, int arg2) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean getScrollableTracksViewportHeight() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean getScrollableTracksViewportWidth() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int getScrollableUnitIncrement(Rectangle arg0, int arg1, int arg2) {
+		// TODO Auto-generated method stub
+		return 0;
 	}
 	
 
